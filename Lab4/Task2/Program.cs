08fb514a3e3e25a64b20df2ef0bc02df91184a1e
Kostya <60173767+KostyaTolok@users.noreply.github.com>
2020-04-18 12:11:13 +0300

    Add files via upload

---
+++ Lab4/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+namespace Task1
+{
+    class Program
+    {
+        [UnmanagedFunctionPointer(CallingConvention.StdCall,CharSet=CharSet.Ansi)]
+        public delegate bool Output(string value);
+        [DllImport("C:\\Homework\\2 semester\\Isp\\UnmanagedDLL\\Debug\\TestDLL.dll", CallingConvention = CallingConvention.StdCall)]
+        static extern void Print(Output output);
+        static void Main(string[] args)
+        {
+            try
+            {
+                Print(PrintToConsole);
+            }
+            catch (DllNotFoundException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            catch(BadImageFormatException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        public static bool PrintToConsole(string value)
+        {
+            Console.WriteLine(value);
+            return true;
+        }
+    }
+}
---
+++ Lab4/Task2/dllmain.cpp
@@ -0,0 +1,31 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include <Windows.h>
+#include <stdio.h>
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+typedef void(__stdcall* Output)(const char*);
+extern "C" __declspec(dllexport) void __stdcall Print(Output output)
+{
+    if (!output)
+    {
+        return;
+    }
+    char buff[64];
+    sprintf_s(buff, "Hello from C++");
+    output(buff);
+}
+
